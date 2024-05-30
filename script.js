a8871e7e34e93d126071a9500fd5fd7f1513fee0
Luke Liu <chocopikachu752@gmail.com>
2024-05-30 21:38:14 +0000

    Updated

--- script.js
+++ script.js
@@ -3,28 +3,34 @@ function getRandomIndex() {
 }
 
 let aiImageIndex;
+const imageIds = ['image1', 'image2', 'image3'];
 
 function startGame() {
-    document.getElementById("titleButton").style.display = "none";
+    document.getElementById("titleButton").hidden = true;
     document.getElementById("titleLabel").style.top = "15%";
     nextRound()
 }
 
 function nextRound() {
-    ['image1', 'image2', 'image3'].forEach((imageId) => {
-        document.getElementById(imageId).src = "none";
-        document.getElementById(imageId).style.border = "none";
+    document.getElementById("nextButton").hidden = true;
+
+    imageIds.forEach((imageId) => {
+        document.getElementById(imageId).parentElement.hidden = true;
     });
 
     aiImageIndex = Math.floor(Math.random() * 3);
     const directories = ["real_images", "real_images", "real_images"];
     directories[aiImageIndex] = "ai_images";
+    
+    imageIds.forEach((imageId, index) => {
+        document.getElementById(imageId).src = `${directories[index]}/person_${getRandomIndex()}.png`;
+        document.getElementById(imageId).style.border = "8px solid #1e1e1e";
+    });
 
-    ['image1', 'image2', 'image3'].forEach((imageId, index) => {
+    imageIds.forEach((imageId, index) => {
         setTimeout(() => {
-            document.getElementById(imageId).src = `${directories[index]}/person_${getRandomIndex()}.png`;
-            document.getElementById(imageId).style.border = "8px solid #1e1e1e";
-        }, index * 333);
+            document.getElementById(imageId).parentElement.hidden = false;
+        }, index * 500);
     });
 }
 
@@ -35,13 +41,15 @@ function isDeepfake(imageId) {
 function selectImage(imageId) {
     const selectedImage = document.getElementById('image' + imageId);
 
-    ['image1', 'image2', 'image3'].forEach((imageId) => {
-        document.getElementById(imageId).parentElement.style.transform = "none";
+    imageIds.forEach((imageId) => {
+        document.getElementById(imageId).parentElement.style.transform = "";
     });
 
     if (isDeepfake(imageId)) {
-        selectedImage.style.border = "8px solid green";
+        selectedImage.style.border = "8px solid #04AA6D";
     } else {
-        selectedImage.style.border = "8px solid red";
+        selectedImage.style.border = "8px solid crimson";
     }
+
+    document.getElementById("nextButton").hidden = false;
 }
