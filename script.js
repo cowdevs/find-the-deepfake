0911f657c942bd4b0691d88b7530138033d067f6
Luke L <chocopikachu752@gmail.com>
2024-05-30 23:21:48 -0700

    New real images

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const imageIds = ['image1', 'image2', 'image3'];
 function cropImage(url, callback) {
     const img = new Image();
     img.crossOrigin = "anonymous";  // This enables CORS
-    img.src = url;
+    img.src = 'http://localhost:3000/proxy?url=' + encodeURIComponent(url);
     img.onload = function() {
         const canvas = document.createElement('canvas');
         canvas.width = 512;
@@ -30,24 +30,41 @@ function nextRound() {
     document.getElementById("nextButton").hidden = true;
 
     aiImageIndex = Math.floor(Math.random() * 3);
+    let imagesLoaded = 0;
     for (let i = 0; i < 3; i++) {
         const imageId = "image" + (i + 1);
         document.getElementById(imageId).parentElement.hidden = true;
         if (i === aiImageIndex) {
             cropImage('https://thispersondoesnotexist.com', function(croppedImageUrl) {
                 document.getElementById(imageId).src = croppedImageUrl;
+                document.getElementById(imageId).onload = function() {
+                    imagesLoaded++;
+                    if (imagesLoaded === 3) {
+                        imageIds.forEach((imageId, index) => {
+                            setTimeout(() => {
+                                document.getElementById(imageId).parentElement.hidden = false;
+                            }, index * 500);
+                        });
+                    }
+                };
             });
         } else {
-            document.getElementById(imageId).src = `real_images/person_${getRandomIndex()}.png`;
+            const img = new Image();
+            img.src = `real_images/person${getRandomIndex()}.jpg`;
+            img.onload = function() {
+                document.getElementById(imageId).src = this.src;
+                imagesLoaded++;
+                if (imagesLoaded === 3) {
+                    imageIds.forEach((imageId, index) => {
+                        setTimeout(() => {
+                            document.getElementById(imageId).parentElement.hidden = false;
+                        }, index * 500);
+                    });
+                }
+            };
         }
         document.getElementById(imageId).style.border = "8px solid #1e1e1e";
     }
-
-    imageIds.forEach((imageId, index) => {
-        setTimeout(() => {
-            document.getElementById(imageId).parentElement.hidden = false;
-        }, index * 500);
-    });
 }
 
 function selectImage(imageId) {
