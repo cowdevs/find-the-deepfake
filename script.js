8a95f148dd67bde6b59ed8acd8114f13c4032237
Luke L <chocopikachu752@gmail.com>
2024-06-05 02:09:22 -0700

    Implemented difficulties and countdown timer *Difficulty selection menu needs styling*

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-let numLives = 3;
+let numLives;
+let countdownSeconds;
 let numCorrect = 0;
-
-let aiImageIndex;
 
 function loadHearts() {
     for (let i = 0; i < numLives; i++) {
@@ -15,9 +14,50 @@ function loadHearts() {
 }
 
 function startGame() {
-    nextRound();
+    const difficulty = getStoredDifficulty();
+    setRules(difficulty)
     loadHearts();
+    // loadTimer();
+    nextRound();
 }
+
+function setRules(difficulty) {
+    if (difficulty === "easy") {
+        numLives = 5;
+        countdownSeconds = 0;
+    } else if (difficulty === "medium") {
+        numLives = 3;
+        countdownSeconds = 0;
+    } else if (difficulty === "hard") {
+        numLives = 3;
+        countdownSeconds = 3;
+    } else {
+        numLives = 1;
+        countdownSeconds = 3;
+    }
+    console.log("Difficulty: " + difficulty);
+    console.log("Lives: " + numLives);
+}
+
+function storeDifficulty() {
+    const selectElement = document.getElementById("difficulties");
+    localStorage.setItem('difficulty', selectElement.value);
+}
+
+function getStoredDifficulty() {
+    const storedDifficulty = localStorage.getItem('difficulty');
+    const selectElement = document.getElementById("difficulties");
+
+    if (storedDifficulty) {
+        selectElement.value = storedDifficulty;
+    } else {
+        selectElement.value = "medium";
+    }
+
+    return storedDifficulty;
+}
+
+let aiImageIndex;
 
 function getRandomImage() {
     const randomFolder = ("00" + Math.floor(Math.random() * 70)).slice(-2) + "000";
@@ -44,7 +84,83 @@ function revealImages() {
             const imageId = "image" + (i + 1);
             document.getElementById(imageId).style.pointerEvents = "auto";
         }
+        if (countdownSeconds > 0) {
+            startCountdown();
+        }
     });
+}
+
+let progressInterval;
+
+function startCountdown() {
+    const countdownBar = document.getElementById("countdownBar");
+    countdownBar.max = (countdownSeconds) * 1000;
+    countdownBar.hidden = false;
+    countdownBar.value = 0;
+
+    progressInterval = setInterval(function() {
+        if (countdownBar.value < countdownBar.max) {
+            countdownBar.value += 5;
+        } else {
+            clearInterval(progressInterval);
+            countdownTimeout();
+        }
+    }, 5);
+}
+
+function countdownTimeout() {
+    document.getElementById("countdownBar").hidden = true;
+
+    numLives--;
+    document.getElementById("heart" + (numLives + 1)).classList.add("lost")
+
+    for (let i = 0; i < 3; i++) {
+        const imageId = "image" + (i + 1);
+        if (i === aiImageIndex) {
+            document.getElementById(imageId).style.border = "0.4vw solid #04AA6D";
+        } else {
+            document.getElementById(imageId).style.border = "0.4vw solid crimson";
+        }
+    }
+
+    if (numLives === 0) {
+        document.getElementById("nextButton").innerText = "Finish";
+        document.getElementById("nextButton").onclick = endGame;
+    }
+    document.getElementById("nextButton").hidden = false;
+}
+
+function selectImage(selectedImageID) {
+    clearInterval(progressInterval);
+    document.getElementById("countdownBar").hidden = true;
+
+    if (selectedImageID === "image" + (aiImageIndex + 1)) {
+        numCorrect++;
+    } else {
+        numLives--;
+        document.getElementById("heart" + (numLives + 1)).classList.add("lost")
+    }
+
+    for (let i = 0; i < 3; i++) {
+        const imageId = "image" + (i + 1);
+        if (imageId !== selectedImageID) {
+            document.getElementById(imageId).style.transform = "";
+            document.getElementById(imageId).style.pointerEvents = "none";
+        }
+        if (i === aiImageIndex) {
+            document.getElementById(imageId).style.border = "0.4vw solid #04AA6D";
+        } else {
+            document.getElementById(imageId).style.border = "0.4vw solid crimson";
+        }
+    }
+
+    document.getElementById(selectedImageID).style.transform = "scale(1.1)";
+
+    if (numLives === 0) {
+        document.getElementById("nextButton").innerText = "Finish";
+        document.getElementById("nextButton").onclick = endGame;
+    }
+    document.getElementById("nextButton").hidden = false;
 }
 
 function loadImage(imageId, src, callback) {
@@ -100,38 +216,16 @@ function nextRound() {
     }
 }
 
-function selectImage(selectedImageID) {
-    if (selectedImageID === "image" + (aiImageIndex + 1)) {
-        numCorrect++;
-    } else {
-        numLives--;
-        document.getElementById("heart" + (numLives + 1)).classList.add("lost")
-    }
 
-    for (let i = 0; i < 3; i++) {
-        const imageId = "image" + (i + 1);
-        if (imageId !== selectedImageID) {
-            document.getElementById(imageId).style.transform = "";
-            document.getElementById(imageId).style.pointerEvents = "none";
-        }
-        if (i === aiImageIndex) {
-            document.getElementById(imageId).style.border = "0.4vw solid #04AA6D";
-        } else {
-            document.getElementById(imageId).style.border = "0.4vw solid crimson";
-        }
-    }
 
-    document.getElementById(selectedImageID).style.transform = "scale(1.1)";
-
-    if (numLives === 0) {
-        document.getElementById("finishButton").hidden = false;
-    } else {
-        document.getElementById("nextButton").hidden = false;
-    }
-}
+let initialDifficulty;
 
 function openPopup(popupID) {
     const popup = document.getElementById(popupID);
+    if (popupID === 'settingsPopup') {
+        const selectElement = document.getElementById("difficulties");
+        initialDifficulty = selectElement.value;
+    }
     popup.style.display = 'flex';
     setTimeout(() => {
         popup.classList.add('show');
@@ -140,6 +234,10 @@ function openPopup(popupID) {
 
 function closePopup(popupID) {
     const popup = document.getElementById(popupID);
+    if (popupID === 'settingsPopup') {
+        const selectElement = document.getElementById("difficulties");
+        selectElement.value = initialDifficulty;
+    }
     popup.classList.remove('show');
     popup.addEventListener('transitionend', function handler() {
         popup.style.display = 'none';
@@ -147,7 +245,34 @@ function closePopup(popupID) {
     });
 }
 
+function startNewGame() {
+    storeDifficulty()
+    window.location.href = "game.html";
+}
+
+function rankFormula(score, numRanks) {
+    return Math.floor(-numRanks * Math.pow(0.9, score) + numRanks)
+}
+
 function endGame() {
-    localStorage.setItem('score', numCorrect);
+    const ranks = ["Newbie", "Amateur", "Expert", "Master"];
+    const numRanks = ranks.length;
+    let multiplier;
+    const difficulty = localStorage.getItem('difficulty');
+
+    if (difficulty === "easy") {
+        multiplier = (numRanks - 1) / numRanks;
+    } else if (difficulty === "medium") {
+        multiplier = 1;
+    } else if (difficulty === "hard") {
+        multiplier = (numRanks + 1) / numRanks;
+    } else {
+        multiplier = (numRanks + 2) / numRanks;
+    }
+
+    const score = numCorrect;
+    const rank = ranks[rankFormula(multiplier * score, numRanks)];
+    localStorage.setItem('score', score);
+    localStorage.setItem('rank', rank);
     window.location.href = "end.html";
 }
